13552b35290b511f41d711a9e987d1d2b8a00b97
ikierepka <ilona.kierepka@gmail.com>
2022-03-04 08:50:37 +0100

    "add EX1 and EX2"

---
+++ src/test/java/Ex1/Main.java
@@ -0,0 +1,29 @@
+package Ex1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+
+
+    public static void main(String args[]) {
+        int numberToGuess = new Random().nextInt(100);
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Choose number between 0 and 100");
+            int choice = scanner.nextInt();
+            if (choice == numberToGuess) {
+                System.out.println("You guessed it!");
+                break;
+            } else if (choice > numberToGuess) {
+                System.out.println("Your number is GREATER than the one you are trying to guess");
+            } else if (choice < numberToGuess) {
+                System.out.println("Your number is LOWER than the one you are trying to guess");
+            }
+        }
+        System.out.println("Sorry you didn't guess the number, the answer was: " + numberToGuess);
+    }
+}
+
---
+++ src/test/java/Ex2/Main.java
@@ -0,0 +1,32 @@
+package Ex2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert size of the table");
+        int size = scanner.nextInt();
+        int[] numbers = new int[size];
+        System.out.println("Table size is " + size);
+
+        int maxNumber = 0;
+        int sum = 0;
+        int counter = numbers.length;
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Insert number " + counter);
+            numbers[i] = scanner.nextInt();
+            sum += numbers[i];
+            maxNumber = numbers[i];
+            if (numbers[i] > maxNumber) {
+                maxNumber = numbers[1];
+            }
+            counter--;
+        }
+        double average = sum/size;
+        System.out.println("The average of your numbers was: " + average);
+        System.out.println("Your highest number was: " + maxNumber);
+    }
+}
